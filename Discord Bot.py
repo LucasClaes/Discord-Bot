562362fe25cd77a985075d319c4157c6f6b9d121
Lucas Claes <lucas.claes@outlook.com>
2023-03-17 21:39:03 +0100

    clear command

--- Discord Bot.py
+++ Discord Bot.py
@@ -23,17 +23,32 @@ def Main():
             print(e)
 
     @bot.tree.command(name="spam")
-    @app_commands.describe(persoon="Welke persoon ik moet spammen")
-    @app_commands.describe(keer="Hoeveel keer ik deze persoon moet spammen")
+    @app_commands.describe(persoon="Welke persoon ik moet spammen.")
+    @app_commands.describe(keer="Hoeveel keer ik deze persoon moet spammen.")
     async def spam(interaction: discord.Interaction, persoon: str, keer: int):
         await interaction.response.send_message(f"Spamming {persoon} {keer}x",ephemeral =True)
         user = user_ids.get(persoon.lower())
         if user is None:
-            await interaction.response.send_message(f"Unknown person: {persoon}")
+            await interaction.response.send_message(f"Unknown person: {persoon}", )
             return
         channel = interaction.channel
+        print(f"Spamming {persoon} {keer}x in opdracht van {interaction.user.name}.")
         for i in range(keer):
             await channel.send(f"<@{user}>")
+
+    @bot.tree.command(name="clear")
+    @app_commands.describe(ammount="Hoeveel berichten ik moet verwijderen")
+    async def clear(interaction: discord.Interaction, ammount: int):
+        await interaction.response.send_message(f"Clearing {ammount} bericht.",ephemeral =True)
+        await interaction.channel.purge(limit=ammount)
+
+
+    @bot.event
+    async def on_message(message):
+        await bot.process_commands(message) # add this if also using command decorators
+        if message.author.id == 1084555252365283441 and "@" in message.content.lower():
+            await message.delete()
+
 
     bot.run(TOKEN)
 
